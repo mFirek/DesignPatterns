c0a580c17566c7a007da107c27c3a4d0352cbc58
Firek <93675817+mFirek@users.noreply.github.com>
2023-06-01 16:20:54 +0200

    Add files via upload

---
+++ Most/Most/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+public interface ITelewizor
+{
+    int Kanal { get; set; }
+
+    void Wlacz();
+    void Wylacz();
+    void ZmienKanal(int kanal);
+    void GetKanal();
+}
+
+
+public class TvLg : ITelewizor
+{
+    public TvLg()
+    {
+        Kanal = 1;
+    }
+
+    public int Kanal { get; set; }
+
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor LG - włączam się.");
+    }
+
+    public void Wylacz()
+    {
+        Console.WriteLine("Telewizor LG - wyłączam się.");
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        Console.WriteLine($"Telewizor LG - zmieniam kanał: {kanal}");
+        Kanal = kanal;
+    }
+
+    public void GetKanal()
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Sprawdź kanał - bieżący kanał: {Kanal}");
+        Console.WriteLine();
+    }
+}
+
+
+public class TvXiaomi : ITelewizor
+{
+    public TvXiaomi()
+    {
+        Kanal = 1;
+    }
+
+    public int Kanal { get; set; }
+
+    public void Wlacz()
+    {
+        Console.WriteLine("Telewizor Xiaomi - włączam się.");
+    }
+
+    public void Wylacz()
+    {
+        Console.WriteLine("Telewizor Xiaomi - wyłączam się.");
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        Console.WriteLine($"Telewizor Xiaomi - zmieniam kanał: {kanal}");
+        Kanal = kanal;
+    }
+
+    public void GetKanal()
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Sprawdź kanał - bieżący kanał: {Kanal}");
+        Console.WriteLine();
+    }
+}
+
+
+public abstract class PilotAbstrakcyjny
+{
+    private ITelewizor tv;
+
+    public PilotAbstrakcyjny(ITelewizor tv)
+    {
+        this.tv = tv;
+    }
+
+    public void Wlacz()
+    {
+        tv.Wlacz();
+    }
+
+    public void Wylacz()
+    {
+        tv.Wylacz();
+    }
+
+    public void ZmienKanal(int kanal)
+    {
+        tv.ZmienKanal(kanal);
+    }
+}
+
+
+public class PilotHarmony : PilotAbstrakcyjny
+{
+    public PilotHarmony(ITelewizor tv) : base(tv)
+    {
+    }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot Harmony - włącz telewizor...");
+        Wlacz();
+    }
+
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot Harmony - wyłącz telewizor...");
+        Wylacz();
+    }
+
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine("Pilot Harmony - zmienia kanał...");
+        ZmienKanal(kanal);
+    }
+}
+
+public class PilotLg : PilotAbstrakcyjny
+{
+    public PilotLg(ITelewizor tv) : base(tv)
+    {
+    }
+
+    public void DoWlacz()
+    {
+        Console.WriteLine("Pilot LG - włącz telewizor...");
+        Wlacz();
+    }
+
+    public void DoWylacz()
+    {
+        Console.WriteLine("Pilot LG - wyłącz telewizor...");
+        Wylacz();
+    }
+
+    public void DoZmienKanal(int kanal)
+    {
+        Console.WriteLine("Pilot LG - zmienia kanał...");
+        ZmienKanal(kanal);
+    }
+}
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        ITelewizor tv = new TvLg();
+        PilotHarmony pilotHarmony = new PilotHarmony(tv);
+        PilotLg pilotLg = new PilotLg(tv);
+
+        pilotHarmony.DoWlacz();
+        tv.GetKanal();
+        pilotLg.DoZmienKanal(100);
+        tv.GetKanal();
+        pilotHarmony.DoWylacz();
+    }
+}
---
+++ Pamiatka/Pamiatka/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pamiatka
+{
+    public interface IMovie
+    {
+        public void SetYear(int year);
+        public IMemento Save();
+        public void Restore(IMemento memento);
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    public interface IMemento
+    {
+        public int GetYear();
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    internal sealed class BackToTheFuture : IMovie
+    {
+        private int Year;
+
+        public BackToTheFuture(int year)
+        {
+            Year = year;
+            Console.WriteLine("Początkowy rok: " + year);
+        }
+
+        public void SetYear(int year)
+        {
+            Year = year;
+            Console.WriteLine("Rok zmieniony na: " + year);
+        }
+
+        public IMemento Save()
+        {
+            Console.WriteLine("Zapisano pamiątkę z roku: " + Year);
+            return new Memento(Year);
+        }
+
+        public void Restore(IMemento memento)
+        {
+            Year = memento.GetYear();
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    internal sealed class Memento : IMemento
+    {
+        private int Year;
+
+        public Memento(int year)
+        {
+            Year = year;
+        }
+
+        public int GetYear()
+        {
+            return Year;
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    internal sealed class Caretaker
+    {
+        private List<IMemento> Mementos = new List<IMemento>();
+
+        private IMovie movie;
+
+        public Caretaker(IMovie movie)
+        {
+            this.movie = movie;
+        }
+
+        public void Save()
+        {
+            Mementos.Add(movie.Save());
+        }
+
+        public void Undo()
+        {
+            if (Mementos.Count == 0)
+            {
+                Console.WriteLine("Nie można cofnąć - brak zapisanych danych");
+                return;
+            }
+
+            var memento = Mementos[Mementos.Count - 1];
+
+            Mementos.RemoveAt(Mementos.Count - 1);
+            movie.Restore(memento);
+
+            Console.WriteLine("Przywrócony rok: " + memento.GetYear());
+        }
+    }
+    //////////////////////////////////////////////////////////////////////////////////////
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackToTheFuture favoriteMovie = new BackToTheFuture(1985);
+            Caretaker caretaker = new Caretaker(favoriteMovie);
+
+            caretaker.Undo();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Część I:");
+            favoriteMovie.SetYear(1955);
+            caretaker.Save();
+            favoriteMovie.SetYear(1985);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Część II:");
+            favoriteMovie.SetYear(2015);
+            favoriteMovie.SetYear(1985);
+            caretaker.Undo();
+            favoriteMovie.SetYear(1985);
+            caretaker.Save();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Część III:");
+            favoriteMovie.SetYear(1885);
+            caretaker.Undo();
+        }
+    }
+}
