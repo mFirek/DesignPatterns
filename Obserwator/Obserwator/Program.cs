bde2f4c375a9f9f1468e68bc843b30381371a800
Firek <93675817+mFirek@users.noreply.github.com>
2023-06-01 16:24:10 +0200

    Add files via upload

---
+++ Obserwator/Obserwator/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Obserwator
+{
+    public enum Genre
+    {
+        Sport,
+        Politics,
+        Economy,
+        Science
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    public interface IObserver
+    {
+        void Update(ISubject subject);
+    }
+
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify();
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    public class NewsAgency : ISubject
+    {
+        public string NewsHeadline;
+        public Genre State;
+
+        public void SetNewsHeadline(Genre state, string news)
+        {
+            State = state;
+            NewsHeadline = news;
+            Notify();
+        }
+
+        private List<IObserver> Observers = new List<IObserver>();
+
+        public void Detach(IObserver observer)
+        {
+            Observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in Observers)
+            {
+                observer.Update(this);
+            }
+        }
+
+        public void Attach(IObserver observer)
+        {
+            Observers.Add(observer);
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    class DailyEconomy : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            var s = subject as NewsAgency;
+            if (s.State == Genre.Economy)
+            {
+                Console.WriteLine($"DailyEconomy publikuje artykuł \"{(subject as NewsAgency).NewsHeadline}\"");
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    class NewYorkTimes : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            var s = subject as NewsAgency;
+            if (s.State == Genre.Politics || s.State == Genre.Sport || s.State == Genre.Economy || s.State == Genre.Science)
+            {
+                Console.WriteLine($"NewYorkTimes publikuje artykuł \"{(subject as NewsAgency).NewsHeadline}\"");
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    class NationalGeographic : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            var s = subject as NewsAgency;
+            if (s.State == Genre.Science)
+            {
+                Console.WriteLine($"NationalGeographic publikuje artykuł \"{(subject as NewsAgency).NewsHeadline}\"");
+            }
+        }
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newsAgency = new NewsAgency();
+
+            var dailyEconomy = new DailyEconomy();
+            var newYork = new NewYorkTimes();
+            var nationalGeographic = new NationalGeographic();
+
+            newsAgency.Attach(dailyEconomy);
+            newsAgency.Attach(newYork);
+            newsAgency.Attach(nationalGeographic);
+
+            newsAgency.SetNewsHeadline(Genre.Economy, "USA is going bancrupt!");
+            newsAgency.SetNewsHeadline(Genre.Science, "Life on Alpha Centauri");
+            newsAgency.SetNewsHeadline(Genre.Sport, "Adam Małysz is the greatest sportsman in the history of mankind");
+            newsAgency.SetNewsHeadline(Genre.Economy, "CD Project RED value has grown by 500% in 2020");
+            newsAgency.SetNewsHeadline(Genre.Science, "Kirkendall effect causing airplanes' engine deteriorate");
+
+            newsAgency.Detach(dailyEconomy);
+
+            newsAgency.SetNewsHeadline(Genre.Economy, "Texas is going bancrupt!");
+
+            newsAgency.Detach(newYork);
+            newsAgency.Detach(nationalGeographic);
+        }
+    }
+}
---
+++ metodaSzablonowa/metodaSzablonowa/Program.cs
@@ -0,0 +1,93 @@
+﻿
+abstract class ZamowienieTemplatka
+{
+
+    public virtual void doKoszyk()
+    {
+        Console.WriteLine("Wybranie produktów...");
+    }
+    public virtual void doPlatnosc()
+    {
+        Console.WriteLine("Płatność w kasie (karta/gotówka)...");
+    }
+
+    public virtual void doDostawa()
+    {
+        Console.WriteLine("Wydanie produktów (odbiór osobisty)...");
+    }
+
+    public virtual void dodanieGratisu()
+    {
+        Console.WriteLine("Dodano gratis...");
+    }
+
+    public void przetwarzajZamowienie(bool czyGratis)
+    {
+        doKoszyk();
+        doPlatnosc();
+        if (czyGratis == true)
+            dodanieGratisu();
+        doDostawa();
+
+    }
+}
+
+
+class ZamowienieOnline : ZamowienieTemplatka
+{
+
+    override public void doKoszyk()
+    {
+        Console.WriteLine("Kompletowanie zamówienia...");
+        Console.WriteLine("Ustawiono parametry wysyłki...");
+    }
+
+    override public void doPlatnosc()
+    {
+        Console.WriteLine("Płatność...");
+    }
+
+    override public void doDostawa()
+    {
+        Console.WriteLine("Wysyłka...");
+    }
+
+}
+
+class ZamowienieStacjonarne : ZamowienieTemplatka
+{
+
+    override public void doKoszyk()
+    {
+        Console.WriteLine("Wybranie produktów...");
+
+    }
+
+    override public void doPlatnosc()
+    {
+        Console.WriteLine("Płatność w kasie (karta/gotówka)...");
+    }
+
+    override public void doDostawa()
+    {
+        Console.WriteLine("Wydanie produktów (odbiór osobisty)...");
+    }
+
+}
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+
+        ZamowienieTemplatka zamowienieOnline = new ZamowienieOnline();
+        zamowienieOnline.przetwarzajZamowienie(true);
+        Console.WriteLine();
+
+        ZamowienieTemplatka zamowienieStacjonarne = new ZamowienieStacjonarne();
+        zamowienieStacjonarne.przetwarzajZamowienie(false);
+
+
+
+    }
+}
